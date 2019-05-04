041b6e27fa172f83c2e359da0907d6e99ac45cec
Pedro Cruz <pedrocruzmx97@outlook.com>
2019-05-04 01:58:47 -0500

    Se modifica la cadena de conexión

--- Entities/Autor.cs
+++ Entities/Autor.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace APIBooks.Entities
 {
     public class Autor
     {
-        public Autor()
-        {
-        }
+        [Key]
+        public int Id { get; set; }
+        public int Nombre { get; set; }
     }
 }
