83c5063770a72755507eef463d29c314cc02c5e9
Pedro Cruz <pedrocruzmx97@outlook.com>
2019-05-04 02:18:50 -0500

    Se inyecta el contexto de la BD y se agregan las operaciones GET del controlador Autor

--- Controllers/AutoresController.cs
+++ Controllers/AutoresController.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using APIBooks.Contexts;
+using APIBooks.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APIBooks.Controllers
@@ -7,6 +12,30 @@ namespace APIBooks.Controllers
     [Route("api/[controller]")]
     public class AutoresController:ControllerBase
     {
+        private readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Autor>> Get()
+        {
+            return context.Autores.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Autor> Get(int id)
+        {
+            var autor = context.Autores.FirstOrDefault(x => x.Id == id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            return autor;
+        }
 
     }
 }
